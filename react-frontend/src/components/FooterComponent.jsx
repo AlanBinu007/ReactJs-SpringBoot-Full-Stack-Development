55895505e5c16928a0bd00564db638903ee42757
Alan Binu <65335042+AlanBinu007@users.noreply.github.com>
2021-12-11 20:12:32 +0530

    Edited the header and footer to make it neet and clean

--- react-frontend/src/components/FooterComponent.jsx
+++ react-frontend/src/components/FooterComponent.jsx
@@ -13,7 +13,7 @@ class FooterComponent extends Component {
         return (
             <div>
                 <footer className = "footer">
-                    <span className="text-muted">All Rights Reserved 2020 @JavaGuides</span>
+                    <span className="text-muted">Designed by Alan</span>
                 </footer>
             </div>
         )
--- react-frontend/src/components/HeaderComponent.js
+++ react-frontend/src/components/HeaderComponent.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class HeaderComponent extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-                 
-        }
-    }
+    this.state = {};
+  }
 
-    render() {
-        return (
-            <div>
-                <header>
-                    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a href="https://javaguides.net" className="navbar-brand">Employee Management App</a></div>
-                    </nav>
-                </header>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <header>
+          <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+            <div>Employee Management App</div>
+          </nav>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default HeaderComponent
+export default HeaderComponent;
