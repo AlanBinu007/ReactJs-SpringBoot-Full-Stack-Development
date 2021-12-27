de542a1a4f293376ab368016c54b98a0c23a2a01
Nikhil Sreeni <96715834+nikhilsreeni29@users.noreply.github.com>
2021-12-27 12:21:59 +0530

    Update HelloWorldService.js

--- spring-boot-react-hello-world-with-routing/frontend/src/service/HelloWorldService.js
+++ spring-boot-react-hello-world-with-routing/frontend/src/service/HelloWorldService.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 class HelloWorldService {
+    
+    //here http://localhost:8080/ replace it whith your spring boot address
 
     executeHelloWorldService() {
         return axios.get('http://localhost:8080/hello-world');
